e4cf5205799bdb416d21051f88f77864b7aa5510
Geoff Whatley <me@geoffwhatley.com>
2017-05-09 10:08:54 +1000

    (fix) Do not throw when passed props with undefined values

--- dist/index.js
+++ dist/index.js
@@ -35,7 +35,7 @@ exports.options = options;
 function doSetAttribute(el, props, propName) {
   if (propName === 'className') {
     el.setAttribute('class', props.className);
-  } else if (props[propName].constructor === Function) {
+  } else if (typeof props[propName] === 'undefined' || props[propName].constructor === Function) {
     return;
   } else {
     el.setAttribute(propName, props[propName]);
